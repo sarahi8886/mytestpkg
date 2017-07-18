3afd769e17248f2ecba61ee0bfedbe1a1dd39af8
sarahi8886 <sarah.isselhorst@fu-berlin.de>
2017-07-18 16:27:30 +0200

    Initial, created fn, test, looking good

---
+++ R/hello.R
@@ -0,0 +1,43 @@
+
+# 1. create analysis dir
+# 2. create RMD in that dir
+# 3. use bookdown to get cap/cross refs
+# 4. write a fn in R/
+# 4a. document your fn with oxygen
+# 5. use the fn in your rmd
+
+
+
+#' Challenge Function
+#'
+#' @param x a nummeric value
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' Chal_func(15)         ##dieser Teil wurde in den Markdown "Challenge" eingebunden
+#'
+#'
+Chal_func <- function(x){
+
+  k =  x/10
+
+  return(k)
+
+}
+
+
+Chal_func(15)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tests/testthat/tests.R
@@ -0,0 +1,11 @@
+library("mytestpkg")
+
+context("the basic text")
+
+test_9 <- Chal_func(15)
+
+test_that("my function works", {
+
+  expect_equal( test_9, "my function works")
+
+})
