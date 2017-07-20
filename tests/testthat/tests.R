51c12cf492816d81b0997c5b84343d0a70b04756
sarahi8886 <sarah.isselhorst@fu-berlin.de>
2017-07-20 09:41:11 +0200

    trav

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -2,7 +2,7 @@ library("mytestpkg")
 
 context("the basic text")
 
-test_9 <- Chal_func(15)
+test_9 <- as.character(Chal_func(15))
 
 test_that("my function works!", {
 
@@ -10,3 +10,4 @@ test_that("my function works!", {
   expect_equal( test_9, "my function works!")
 
 })
+
